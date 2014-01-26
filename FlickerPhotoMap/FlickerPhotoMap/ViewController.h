fe88b3a846c414879a49d8ee96d48fe0c92edaa3
jjuiddong <jjuiddong@hanmail.net>
2014-01-26 20:37:39 +0900

    add FlickerPhotoMap project

---
+++ FlickerPhotoMap/FlickerPhotoMap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickerPhotoMap
+//
+//  Created by Lee JaeJung on 2014. 1. 26..
+//  Copyright (c) 2014년 Lee JaeJung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FlickerPhotoMap/FlickerPhotoMap/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FlickerPhotoMap
+//
+//  Created by Lee JaeJung on 2014. 1. 26..
+//  Copyright (c) 2014년 Lee JaeJung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
